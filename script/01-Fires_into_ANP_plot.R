c6f94aaf7c7ffed7c131456bc2751d99bfeaacb8
Fernando.Prudencio.Laptop <fjprudenciop@gmail.com>
2020-03-22 15:17:18 -0500

    geopackage data applied

--- script/01-Fires_into_ANP_plot.R
+++ script/01-Fires_into_ANP_plot.R
@@ -1,44 +1,58 @@
+#' @author  Fernando Prudencio
 rm(list = ls())
 library(tidyverse)
-library(spatialEco)
 library(sf)
-library(mapview)
 library(ggplot2)
 
-#'CARGANDO DATOS VECTORIALES
-anp <- read_sf('data/vector/anp/ANP.geojson') %>%
+#' CARGANDO DATOS VECTORIALES
+anp <- st_read("data/vector/anp/ANP.gpkg",
+    layer = "ANP_uptade_2020", quiet = T, as_tibble = T
+  ) %>%
   dplyr::select(anp_cate, anp_nomb)
 
-fires <- 
-  read_sf('data/vector/fire_register/FiresRegister_2000_Dic2019.geojson') %>%
-  dplyr::filter('ANO' != 2019)
+fires <-
+  st_read("data/vector/fire_register/GPKG_FIRES_REGISTER.gpkg",
+    layer = "from_2000_dic2019", quiet = T, as_tibble = T
+  ) %>%
+  dplyr::filter(ANO != 2019)
 
-#'INTERSECTANDO DATOS VECTORIALES
+#' INTERSECTANDO DATOS VECTORIALES
 vctr.inter <- st_intersection(fires, anp)
 
-#'CREANDO TIBBLE PARA EL PLOTEO
-df <- vctr.inter %>% as_tibble() %>% select(anp_cate, anp_nomb, ANO, MES) %>%
-  mutate(nfires = 1) %>% group_by(anp_cate, anp_nomb) %>%
+#' CREANDO TIBBLE PARA EL PLOTEO
+df <- vctr.inter %>%
+  as_tibble() %>%
+  select(anp_cate, anp_nomb) %>%
+  mutate(nfires = 1) %>%
+  group_by(anp_cate, anp_nomb) %>%
   summarise(nfires = sum(nfires)) %>%
-  mutate(nom = sprintf('%1$s %2$s', anp_cate, anp_nomb)) %>%
-  arrange(anp_cate, nfires) %>% ungroup() %>%
-  mutate(sortfield = sprintf('%02d', 1:23))
+  mutate(nom = sprintf("%1$s %2$s", anp_cate, anp_nomb)) %>%
+  arrange(anp_cate, nfires) %>%
+  ungroup() %>%
+  mutate(sortfield = sprintf("%02d", 1:length(nfires)))
 
-#'PLOTEO DE GRAFICO DE BARRAS, NUMERO DE INCENDIOS POR ANP
+#' PLOTEO DE GRAFICO DE BARRAS, NUMERO DE INCENDIOS POR ANP
 plot <- ggplot(df, aes(sortfield, nfires, fill = anp_cate)) +
   geom_bar(stat = "identity", colour = "black") +
-  scale_fill_brewer(palette="Pastel1") +
-  theme_bw() + ylab('Number of fires') + xlab('') + labs(fill = 'ANP') +
-  theme(legend.title  = element_text(size = 20),
-        legend.text   = element_text(size = 15),
-        plot.title    = element_text(size = 20, hjust = 0.5),
-        plot.subtitle = element_text(size = 12, hjust = 0.5),
-        axis.text.x   = element_text(size = 16, angle = 90, hjust = 1),
-        axis.text.y   = element_text(size = 16),
-        axis.title.x  = element_text(size = 15),
-        axis.title.y  = element_text(size = 15)) +
+  scale_fill_brewer(palette = "Pastel1") +
+  theme_bw() +
+  ylab("Number of fires") +
+  xlab("") +
+  labs(fill = "ANP") +
+  theme(
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 15),
+    plot.title = element_text(size = 20, hjust = 0.5),
+    plot.subtitle = element_text(size = 12, hjust = 0.5),
+    axis.text.x = element_text(size = 16, angle = 90, hjust = 1),
+    axis.text.y = element_text(size = 16),
+    axis.title.x = element_text(size = 15),
+    axis.title.y = element_text(size = 15)
+  ) +
   scale_x_discrete(label = df$nom) +
-  scale_y_continuous(breaks = seq(0,20,5), limits = c(0,20))
+  scale_y_continuous(breaks = seq(0, 30, 5), limits = c(0, 30))
 plot
-ggsave(plot, filename = 'exports/Fires_into_ANP_v1.png',
-       width = 30, height = 25, units = "cm", dpi = 1000)
+ggsave(plot,
+  filename = "exports/Fires_into_ANP_v2.png",
+  width = 30, height = 25, units = "cm", dpi = 1000
+)
